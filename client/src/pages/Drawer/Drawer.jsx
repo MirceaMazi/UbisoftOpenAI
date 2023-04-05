3c0c193742ab203c3a0486aa9a2c3889276b68e0
Florin Ruscuta <ruscutaf@gmail.com>
2023-04-06 00:57:09 +0300

    imbunatatita componenta de input

--- client/src/pages/Drawer/Drawer.jsx
+++ client/src/pages/Drawer/Drawer.jsx
@@ -6,7 +6,6 @@ function Drawer() {
   const [prompt, setPrompt] = useState("test");
   return (
     <div className="painter-page-container">
-      <main></main>
       <p className="message">{prompt} aici este promptul</p>
       <Input prompt={prompt} setPrompt={setPrompt}></Input>
     </div>
--- client/src/pages/FortuneTeller/FortuneTeller.jsx
+++ client/src/pages/FortuneTeller/FortuneTeller.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Input from "../../components/Input/Input";
+import "./FortuneTeller.css";
+import ChatPopUp from "../../components/ChatPopUp/ChatPopUp";
+
 function FortuneTeller(props) {
-  return (
-    <div>
-      <Input />
-    </div>
-  );
+    const [prompt, setPrompt] = useState("test");
+    return (
+        <div className="container">
+            <p className="message">{prompt} aici este promptul</p>
+            <Input prompt={prompt} setPrompt={setPrompt}></Input>
+            <footer>
+                <ChatPopUp/>
+            </footer>
+        </div>
+    );
 }
 
 export default FortuneTeller;
